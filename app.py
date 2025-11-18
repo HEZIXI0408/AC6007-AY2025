3b311568654f4bf73c7c40b87f98f4d52c81052d
HEZIXI0408 <he0001xi@ntu.edu.sg>
2025-11-18 03:03:35 +0000

    1

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 #flask
-
 from flask import Flask, render_template, request
-
 app=Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
+    return(render_template("index.html"))
+
+@app.route("/main",methods=["GET","POST"])
+def main():
+    r=request.form.get("q")
     return(render_template("index.html"))
 
 if __name__=="__main__":
